8a3cfcf70864a44928c7a956e8a837a7c246e17e
MattiaRediti <66474326+MattiaRediti@users.noreply.github.com>
2024-03-03 11:51:36 +0100

    Update example_compute_HER2_subtypes.R

--- example_files/example_compute_HER2_subtypes.R
+++ example_files/example_compute_HER2_subtypes.R
@@ -4,9 +4,9 @@
 #### it requires "sigs_groups_class_final.RDS", "median_genes.RDS", "x_mean_genes.RDS", "x_sd_genes.RDS" in your working directory
 
 sigs_groups_class_tot <- readRDS(paste0(getwd(), "/sigs_groups_class_final.RDS"))
-median_genes = readRDS(paste0(getwd(), "/median_genes.RDS"))
-xcenter_genes = readRDS(paste0(getwd(), "/x_mean_genes.RDS"))
-xcenter_sd_genes = readRDS(paste0(getwd(), "/x_sd_genes.RDS"))
+median_genes <- readRDS(paste0(getwd(), "/median_genes.RDS"))
+xcenter_genes <- readRDS(paste0(getwd(), "/x_mean_genes.RDS"))
+xcenter_sd_genes <- readRDS(paste0(getwd(), "/x_sd_genes.RDS"))
 
 
 
